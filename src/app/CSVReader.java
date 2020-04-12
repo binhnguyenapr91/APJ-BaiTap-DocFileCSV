1e4c3ddad2ff4bba08c493cd4664d4b4b151a345
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-04-12 18:05:19 +0700

    init commit

---
+++ src/app/CSVReader.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CSVReader {
+
+    public static void main(String[] args) {
+
+        String csvFile = "E:\\MyProjects\\MyJava\\APJ-BaiTap-DocFileCSV\\file\\countries.txt";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
